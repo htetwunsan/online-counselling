f897a40eb1f801523b0f957fe22c233a8d36371e
htetwunsan <htetwunsan2000@gmail.com>
2022-03-18 23:39:25 +0630

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            RoleAndPermissionSeeder::class
+        ]);
+        $user = User::firstOrCreate([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ]);
+        $user->assignRole('system admin');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('auth')->group(function () {
+    Route::middleware('role:system admin')->prefix('admin')->group(function () {
+        Route::get('/dashboard', DashboardController::class);
+    });
+
+    Route::get('/profile', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
